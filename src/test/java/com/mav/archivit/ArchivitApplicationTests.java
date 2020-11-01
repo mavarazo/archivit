d403bac3b8b43e96c525a1decb757d4b3d769f1d
Mavarazo <marco.niederberger@gmail.com>
2020-11-01 11:06:59 +0100

    !!! Init Archivit 2.0

---
+++ src/test/java/com/mav/archivit/ArchivitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mav.archivit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
