cf7922e0089dced60a8487c4570cce093d08fe97
Mavarazo <marco.niederberger@gmail.com>
2020-12-17 15:12:52 +0100

    !!! logging

--- processor.py
+++ processor.py
@@ -13,6 +13,7 @@ class ArchivitProcessor():
 
 
     def process(self, file_path):
+        self.logger.info(f"{file_path} processing")
         results = list()
         content = self._get_pdf_content(file_path)
         if content:
@@ -20,9 +21,10 @@ class ArchivitProcessor():
                 if rule['keywords']:
                     matches = self._words_in_string(rule['keywords'], content)
                     score = 100 / len(rule['keywords']) * len(matches)
+                    self.logger.info(f"{file_path} {rule['name']} {score}")
                     if score == 100:
-                        return (rule, matches, score)
-                    if score > 64:
+                        return list((rule, matches, score))
+                    elif score > 64:
                         results.append((rule, matches, score))
         
         return results
