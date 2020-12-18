f02b93d45f7e1ab6d0744cbb95addb64740fa1c0
Mavarazo <marco.niederberger@gmail.com>
2020-12-18 08:16:41 +0100

    !!! plain spring boot

---
+++ src/main/java/com/mav/archivit/ArchivitApplication.java
@@ -0,0 +1,13 @@
+package com.mav.archivit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivitApplication.class, args);
+	}
+
+}
